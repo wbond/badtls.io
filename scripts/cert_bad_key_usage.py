8a22243aa527ecf1fde4dd4dacc73b14cf22abbe
Joachim Viide <jviide@iki.fi>
2016-09-06 15:19:05 +0300

    Add subjectAltNames to all server certificates
    Closes #4.

--- scripts/cert_bad_key_usage.py
+++ scripts/cert_bad_key_usage.py
@@ -13,6 +13,7 @@ def generate_bad_key_usage_cert(domain, base_year, quiet=False):
     if not quiet:
         write('Generating bad-key-usage cert ... ', end='')
 
+    full_domain = 'bad-key-usage.{}'.format(domain)
     ca_private_key = load_private('ca')
     ca_cert = load_cert('ca')
     public_key = load_public('host')
@@ -23,11 +24,12 @@ def generate_bad_key_usage_cert(domain, base_year, quiet=False):
             'state_or_province_name': 'Massachusetts',
             'locality_name': 'Newbury',
             'organization_name': 'Bad TLS Limited',
-            'common_name': 'bad-key-usage.{}'.format(domain),
+            'common_name': full_domain,
         },
         public_key
     )
     builder.issuer = ca_cert
+    builder.subject_alt_domains = [full_domain]
     builder.begin_date = datetime(base_year, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
     builder.end_date = datetime(base_year + 3, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
     builder.key_usage = set(['crl_sign'])
