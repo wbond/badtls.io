d0cb3ea887ce5c28aaea025a22c918193c3078ef
Joachim Viide <jviide@iki.fi>
2016-09-10 16:18:11 +0300

    Add no-san subdomain

---
+++ scripts/cert_no_san.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+from __future__ import unicode_literals, division, absolute_import, print_function
+
+from datetime import datetime
+
+from asn1crypto.util import timezone
+from certbuilder import CertificateBuilder
+
+from _util import load_public, load_private, load_cert, dump_cert, write
+
+
+def generate_no_san_cert(domain, base_year, quiet=False):
+    if not quiet:
+        write('Generating domain-match cert ... ', end='')
+
+    ca_private_key = load_private('ca')
+    ca_cert = load_cert('ca')
+    public_key = load_public('host')
+
+    builder = CertificateBuilder(
+        {
+            'country_name': 'US',
+            'state_or_province_name': 'Massachusetts',
+            'locality_name': 'Newbury',
+            'organization_name': 'Bad TLS Limited',
+            'common_name': 'no-san.{}'.format(domain),
+        },
+        public_key
+    )
+    builder.issuer = ca_cert
+    builder.begin_date = datetime(base_year, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
+    builder.end_date = datetime(base_year + 3, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
+    certificate = builder.build(ca_private_key)
+
+    dump_cert('no-san', certificate)
+
+    if not quiet:
+        write('done')
--- scripts/generate.py
+++ scripts/generate.py
@@ -10,6 +10,7 @@ from cert_ca2 import generate_ca2_cert
 from cert_domain_match import generate_domain_match_cert
 from cert_wildcard import generate_wildcard_cert
 from cert_san_match import generate_san_match_cert
+from cert_no_san import generate_no_san_cert
 from cert_expired import generate_expired_cert
 from cert_expired_1963 import generate_expired_1963_cert
 from cert_future import generate_future_cert
@@ -34,6 +35,7 @@ def generate_certs(domain, base_year, quiet=False):
     generate_domain_match_cert(domain, base_year, quiet)
     generate_wildcard_cert(domain, base_year, quiet)
     generate_san_match_cert(domain, base_year, quiet)
+    generate_no_san_cert(domain, base_year, quiet)
     generate_expired_cert(domain, base_year, quiet)
     generate_expired_1963_cert(domain, quiet)
     generate_future_cert(domain, base_year, quiet)
