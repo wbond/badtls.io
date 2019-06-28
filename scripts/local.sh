d1913d9c0ed1aaea356af85176a49a0c3d2d70ca
wbond <will@wbond.net>
2019-06-28 13:27:08 -0400

    Fix RC4 being missing in modern OpenSSL also

--- scripts/local.sh
+++ scripts/local.sh
@@ -35,8 +35,14 @@ sleep 1.5
 $SOCAT tcp:localhost:9990 openssl-listen:10003,reuseaddr,fork,cipher=EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH:DES-CBC3-SHA,cert=./certs/auth.crt,key=./certs/host.key,dhparam=./certs/dhparam.pem,cafile=./certs/ca.crt,verify=1 &
 SOCAT_CLIENT_AUTH_PID=$!
 
-$SOCAT tcp:localhost:9992 openssl-listen:11004,reuseaddr,fork,cipher=EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH:DES-CBC3-SHA,cert=./certs/weak-sig.crt,key=./certs/host.key,dhparam=./certs/dhparam.pem,cafile=./certs/ca.crt,verify=1 &
+$SOCAT tcp:localhost:9992 openssl-listen:11004,reuseaddr,fork,cipher=EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH:DES-CBC3-SHA,cert=./certs/weak-sig.crt,key=./certs/host.key,dhparam=./certs/dhparam.pem,verify=1 &
 SOCAT_MD5_PID=$!
+
+$SOCAT tcp:localhost:9992 openssl-listen:11008,reuseaddr,fork,cipher=RC4,cert=./certs/wildcard.crt,key=./certs/host.key,dhparam=./certs/dhparam.pem,verify=1 &
+SOCAT_RC4_PID=$!
+
+$SOCAT tcp:localhost:9992 openssl-listen:11009,reuseaddr,fork,cipher=RC4-MD5,cert=./certs/wildcard.crt,key=./certs/host.key,dhparam=./certs/dhparam.pem,verify=1 &
+SOCAT_RC4_MD5_PID=$!
 
 wait
 
@@ -44,6 +50,8 @@ cleanup() {
     kill $NGINX_PID 2> /dev/null
     kill $SOCAT_CLIENT_AUTH_PID 2> /dev/null
     kill $SOCAT_MD5_PID 2> /dev/null
+    kill $SOCAT_RC4_PID 2> /dev/null
+    kill $SOCAT_RC4_MD5_PID 2> /dev/null
 }
 
 trap 'cleanup' 1 2 3 15 EXIT
